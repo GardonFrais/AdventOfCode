5b1f00250e01ad9c0af204d944f006b97bf0ae7c
Gardon <131396718+GardonFrais@users.noreply.github.com>
2023-12-02 09:14:54 +0000

    [01/12/2023] Part 2

--- 2023/01/solve.spec.ts
+++ 2023/01/solve.spec.ts
@@ -1,22 +1,42 @@
 const fs = require('fs');
 const solve = require('./solve');
 
-test('solves 01/12/2023 puzzle example', () => {
-  const fileContent = fs.readFileSync(`${__dirname}/input.spec.in`).toString();
-  expect(solve.solve(fileContent)).toBe(142);
+test('solves 01/12/2023 puzzle example 1', () => {
+  const fileContent = fs.readFileSync(`${__dirname}/input.pt1.spec.in`).toString();
+  expect(solve.solve(fileContent, solve.DIGITS)).toBe(142);
 });
 
-test('finds first and last digit in string', () => {
-  const input = 'a1b2c3d4e5f';
-  expect(solve.concatFirstAndLastDigit(input)).toBe(15);
+test('solves 01/12/2023 puzzle example 2', () => {
+  const fileContent = fs.readFileSync(`${__dirname}/input.pt2.spec.in`).toString();
+  expect(solve.solve(fileContent, solve.FULL_TEXT_AND_DIGITS)).toBe(281);
 });
 
-test('finds first and last digit in string if there is only 1 digit', () => {
-  const input = 'treb7uchet';
-  expect(solve.concatFirstAndLastDigit(input)).toBe(77);
+test('finds first digit in string (digit)', () => {
+  const input = 'two1nine';
+  expect(solve.findFirst(input, solve.DIGITS)).toBe('1');
 });
 
-test('finds first and last digit in string if there is only no digit', () => {
+test('finds first digit in string (full text)', () => {
+  const input = 'two1nine';
+  expect(solve.findFirst(input, solve.FULL_TEXT_AND_DIGITS)).toBe('2');
+});
+
+test('finds no first digit', () => {
   const input = 'trebuchet';
-  expect(solve.concatFirstAndLastDigit(input)).toBe(0);
+  expect(solve.findFirst(input, solve.FULL_TEXT_AND_DIGITS)).toBe(undefined);
+});
+
+test('finds last digit in string (digit)', () => {
+  const input = 'two1nine';
+  expect(solve.findLast(input, solve.DIGITS)).toBe('1');
+});
+
+test('finds last digit in string (full text)', () => {
+  const input = 'two1nine';
+  expect(solve.findLast(input, solve.FULL_TEXT_AND_DIGITS)).toBe('9');
+});
+
+test('finds no last digit', () => {
+  const input = 'trebuchet';
+  expect(solve.findLast(input, solve.FULL_TEXT_AND_DIGITS)).toBe(undefined);
 });
--- 2023/01/solve.ts
+++ 2023/01/solve.ts
@@ -1,21 +1,72 @@
 const fs = require('fs');
 
-const DIGITS: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+enum FirstLast {
+  First = 1,
+  Last = -1
+}
 
-export const solve = (data: string): number => {
+export const DIGITS: Record<string, string> = {
+  '0': '0',
+  '1': '1',
+  '2': '2',
+  '3': '3',
+  '4': '4',
+  '5': '5',
+  '6': '6',
+  '7': '7',
+  '8': '8',
+  '9': '9'
+}
+
+const FULL_TEXT: Record<string, string> = {
+  'zero': '0',
+  'one': '1',
+  'two': '2',
+  'three': '3',
+  'four': '4',
+  'five': '5',
+  'six': '6',
+  'seven': '7',
+  'eight': '8',
+  'nine': '9'
+};
+
+export const FULL_TEXT_AND_DIGITS: Record<string, string> = {
+  ...DIGITS,
+  ...FULL_TEXT
+};
+
+export const solve = (data: string, searchedValues: Record<string, string>): number => {
   return data.split('\n')
-    .map(line => concatFirstAndLastDigit(line))
+    .map(line => Number((findFirst(line, searchedValues) ?? '0') + (findLast(line, searchedValues) ?? '0')))
     .reduce((acc, curr) => acc + curr, 0);
 }
 
-export const concatFirstAndLastDigit = (data: string): number => {
-  const filteredData: string[] = [...data].filter(char => DIGITS.includes(Number(char)));
-  return Number(filteredData[0] + filteredData[filteredData.length - 1]) || 0;
+export const findFirst = (data: string, searchedValues: Record<string, string>): string | undefined => {
+  return findDigit(data, searchedValues, FirstLast.First);
 }
 
-const solveFile = (filename: string): number => {
-  const fileContent: string = fs.readFileSync(`${__dirname}/${filename}`).toString();
-  return solve(fileContent);
+export const findLast = (data: string, searchedValues: Record<string, string>): string | undefined => {
+  return findDigit(data, searchedValues, FirstLast.Last);
 }
 
-console.log('input.in result : ', solveFile('input.in'));
+const findDigit = (data: string, searchedValues: Record<string, string>, firstLast: FirstLast): string | undefined => {
+  const start = firstLast === FirstLast.First ? 0 : data.length - 1;
+  const end = firstLast === FirstLast.First ? data.length : -1;
+
+  for (let i = start; i !== end; i += firstLast.valueOf()) {
+    for (const searchedValue in searchedValues) {
+      if (data.slice(i, i + searchedValue.length) === searchedValue) {
+        return searchedValues[searchedValue];
+      }
+    }
+  }
+  return undefined;
+}
+
+const loadFile = (filename: string): string => {
+  return fs.readFileSync(`${__dirname}/${filename}`).toString();
+}
+
+console.log('input.in result 1 : ', solve(loadFile('input.in'), DIGITS));
+console.log('input.in result 2 : ', solve(loadFile('input.in'), FULL_TEXT_AND_DIGITS));
