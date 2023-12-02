7b28a8ca7be7ac77b775cede70306fce5c745ec0
Gardon <131396718+GardonFrais@users.noreply.github.com>
2023-12-02 13:04:48 +0000

    [02/12/2023] Part 2

--- 2023/02/solve.spec.ts
+++ 2023/02/solve.spec.ts
@@ -1,16 +1,52 @@
+import { Draw } from "./solve";
+
 const fs = require('fs');
 const solve = require('./solve');
 
-const BAG_CONTENT: Record<string, number> = {
+const BAG_CONTENT: Draw = {
   'red' : 12,
   'green': 13,
   'blue': 14
 }
 
 test('solves 02/12/2023 puzzle example 1', () => {
-  const fileContent = fs.readFileSync(`${__dirname}/input.pt1.spec.in`).toString();
-  expect(solve.solve(fileContent, 12, 13, 14)).toBe(8);
+  const fileContent = fs.readFileSync(`${__dirname}/input.spec.in`).toString();
+  expect(solve.solve1(fileContent, 12, 13, 14)).toBe(8);
 });
+
+test('solves 02/12/2023 puzzle example 2', () => {
+  const fileContent = fs.readFileSync(`${__dirname}/input.spec.in`).toString();
+  expect(solve.solve2(fileContent)).toBe(2286);
+});
+
+test('parse draw', () => {
+  const input = ' 1 red, 2 green, 6 blue ';
+  expect(solve.parseDraw(input).red).toBe(1);
+  expect(solve.parseDraw(input).green).toBe(2);
+  expect(solve.parseDraw(input).blue).toBe(6);
+});
+
+test('parse partial draw', () => {
+  const input = ' 1 red, 6 blue ';
+  expect(solve.parseDraw(input).red).toBe(1);
+  expect(solve.parseDraw(input).green).toBe(0);
+  expect(solve.parseDraw(input).blue).toBe(6);
+});
+
+test('parse game', () => {
+  const input = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green';
+  expect(solve.parseGame(input)[0]).toBe(1);
+  expect(solve.parseGame(input)[1].length).toBe(3);
+  expect(solve.parseGame(input)[1][0].red).toBe(4);
+  expect(solve.parseGame(input)[1][0].green).toBe(0);
+  expect(solve.parseGame(input)[1][0].blue).toBe(3);
+  expect(solve.parseGame(input)[1][1].red).toBe(1);
+  expect(solve.parseGame(input)[1][1].green).toBe(2);
+  expect(solve.parseGame(input)[1][1].blue).toBe(6);
+  expect(solve.parseGame(input)[1][2].red).toBe(0);
+  expect(solve.parseGame(input)[1][2].green).toBe(2);
+  expect(solve.parseGame(input)[1][2].blue).toBe(0);
+})
 
 test('game is possible', () => {
   const input = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green';
@@ -51,3 +87,14 @@ test('extract game number multiple digits digit', () => {
   const input = 'Game 13: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red';
   expect(solve.extractGameNumber(input)).toBe(13);
 });
+
+test('calc minimal colors to valid game', () => {
+  const input: Draw[] = [
+    { red: 4, green: 0, blue: 3 },
+    { red: 1, green: 2, blue: 6 },
+    { red: 0, green: 2, blue: 0 }
+  ];
+  expect(solve.calcMinimalBag(input).red).toBe(4);
+  expect(solve.calcMinimalBag(input).green).toBe(2);
+  expect(solve.calcMinimalBag(input).blue).toBe(6);
+})
--- 2023/02/solve.ts
+++ 2023/02/solve.ts
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
-export const solve = (data: string, redNumber: number, greenNumber: number, blueNumber: number): number => {
-  const bagContent: Record<string, number> = {
+export type Draw = {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+export const solve1 = (data: string, redNumber: number, greenNumber: number, blueNumber: number): number => {
+  const bagContent: Draw = {
     'red': redNumber,
     'green': greenNumber,
     'blue': blueNumber
@@ -11,19 +17,66 @@ export const solve = (data: string, redNumber: number, greenNumber: number, blue
     .reduce((acc, curr) => acc + curr, 0);
 }
 
-export const isGamePossible = (data: string, bagContent: Record<string, number>): boolean => {
-  console.log(data);
-  return data.split(':')[1]
+export const solve2 = (data: string): number => {
+  return data.split('\n')
+    .map(game => {
+      const [, parsedGame] = parseGame(game);
+      const minimalBag = calcMinimalBag(parsedGame);
+      return minimalBag.red * minimalBag.green * minimalBag.blue;
+    }).reduce((acc, curr) => acc + curr, 0);
+}
+
+export const calcMinimalBag = (game: Draw[]): Draw => {
+  return game.reduce((acc, curr) => ({
+    red: curr.red > acc.red ? curr.red : acc.red,
+    green: curr.green > acc.green ? curr.green : acc.green,
+    blue: curr.blue > acc.blue ? curr.blue : acc.blue
+  }));
+}
+
+export const parseGame = (game: string): [number, Draw[]] => {
+  const [gameId, gameValues] = game.split(':');
+  const draws: string[] = gameValues.split(';').map(draw => draw.trim());
+  return [Number(gameId.split(' ')[1]), draws.map(draw => parseDraw(draw))];
+}
+
+export const parseDraw = (draw: string): Draw => {
+  const result = {
+    red: 0,
+    green: 0,
+    blue: 0
+  };
+
+  const [color1, color2, color3] = draw.split(',').map(color => color.trim());
+  const [color1value, color1key] = (color1?.split(' ') ?? []) as [string, keyof Draw | undefined];
+  const [color2value, color2key] = (color2?.split(' ') ?? []) as [string, keyof Draw | undefined];
+  const [color3value, color3key] = (color3?.split(' ') ?? []) as [string, keyof Draw | undefined];
+
+  if (color1key !== undefined) {
+    result[color1key] = Number(color1value);
+  }
+
+  if (color2key !== undefined) {
+    result[color2key] = Number(color2value);
+  }
+
+  if (color3key !== undefined) {
+    result[color3key] = Number(color3value);
+  }
+
+  return result;
+}
+
+export const isGamePossible = (game: string, bagContent: Draw): boolean => {
+  return game.split(':')[1]
     .split(';')
     .map((draw: string) => isDrawPossible(draw.trim(), bagContent))
     .reduce((acc, curr) => acc && curr, true);
 }
 
-export const isDrawPossible = (data: string, bagContent: Record<string, number>): boolean => {
-  const colors = data.split(',');
-  return colors.map((color: string) => color.trim().split(' '))
-    .filter((color: string[]) => bagContent[color[1]] >= Number(color[0]))
-    .length === colors.length;
+export const isDrawPossible = (draw: string, bagContent: Draw): boolean => {
+  const parsedDraw: Draw = parseDraw(draw);
+  return parsedDraw.red <= bagContent.red && parsedDraw.green <= bagContent.green && parsedDraw.blue <= bagContent.blue;
 }
 
 export const extractGameNumber = (data: string): number => {
@@ -34,4 +87,5 @@ const loadFile = (filename: string): string => {
   return fs.readFileSync(`${__dirname}/${filename}`).toString();
 }
 
-console.log('input.in : ', solve(loadFile('input.in'), 12, 13, 14));
+console.log('input.in pt1 : ', solve1(loadFile('input.in'), 12, 13, 14));
+console.log('input.in pt2 : ', solve2(loadFile('input.in')));
