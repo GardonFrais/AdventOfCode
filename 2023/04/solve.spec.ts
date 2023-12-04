913b93d39cd412b90d25b6e19eb4dcb0d3b02ad7
Gardon <131396718+GardonFrais@users.noreply.github.com>
2023-12-04 12:01:49 +0000

    [04/12/2023] Part 2

--- 2023/04/solve.spec.ts
+++ 2023/04/solve.spec.ts
@@ -3,24 +3,40 @@ import { Scratchcard } from "./solve";
 const fs = require('fs');
 const solve = require('./solve');
 
-test('solves 04/12/2023 puzzle example 1', () => {
+test('solves 04/12/2023 puzzle part 1', () => {
   const fileContent: string = fs.readFileSync(`${__dirname}/input.spec.in`).toString();
-  expect(solve.solve(fileContent)).toBe(13);
+  expect(solve.solve1(fileContent)).toBe(13);
+});
+
+test('solves 04/12/2023 puzzle part 2', () => {
+  const fileContent: string = fs.readFileSync(`${__dirname}/input.spec.in`).toString();
+  expect(solve.solve2(fileContent)).toBe(30);
 });
 
 test('parse to scratchcard', () => {
   const input: string = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53';
   const expectedOutput: Scratchcard = {
+    cardId: 1,
     winningNumbers: [41, 48, 83, 86, 17],
     gameNumbers: [83, 86, 6, 31, 17, 9, 48, 53]
   };
   expect(solve.parseScratchcard(input)).toStrictEqual(expectedOutput);
 });
 
-test('calc card value', () => {
+test('calc card winning numbers', () => {
   const input: Scratchcard = {
+    cardId: 1,
     winningNumbers: [41, 48, 83, 86, 17],
     gameNumbers: [83, 86, 6, 31, 17, 9, 48, 53]
   };
-  expect(solve.calcScratchcardValue(input)).toBe(8);
-})
+  expect(solve.calcScratchcardWinningNumbersCount(input)).toBe(4);
+});
+
+test('calc card ids to duplicate', () => {
+  const input: Scratchcard = {
+    cardId: 1,
+    winningNumbers: [41, 48, 83, 86, 17],
+    gameNumbers: [83, 86, 6, 31, 17, 9, 48, 53]
+  }
+  expect(solve.calcCardIdsToDuplicate(input)).toStrictEqual([2, 3, 4, 5])
+});
--- 2023/04/solve.ts
+++ 2023/04/solve.ts
@@ -1,30 +1,54 @@
 const fs = require('fs');
 
 export type Scratchcard = {
+  cardId: number;
   winningNumbers: number[];
   gameNumbers: number[];
 }
 
-export const solve = (data: string): number => {
+type ScratchcardResolution = {
+  scratchcard: Scratchcard,
+  numberOfOccurences: number
+}
+
+export const solve1 = (data: string): number => {
   return data.split('\n')
-    .map(line => calcScratchcardValue(parseScratchcard(line)))
+    .map(line => calcScratchcardWinningNumbersCount(parseScratchcard(line)))
+    .reduce((acc, curr) => (acc + (curr > 0 ? 2 ** (curr - 1) : 0)), 0);
+}
+
+export const solve2 = (data: string): number => {
+  let scratchcardResolutions: ScratchcardResolution[] = data.split('\n')
+    .map(line => ({ scratchcard: parseScratchcard(line), numberOfOccurences: 1 }));
+
+  for (const scratchcardResolution of scratchcardResolutions) {
+    const cardIdsToDuplicate: number[] = calcCardIdsToDuplicate(scratchcardResolution.scratchcard);
+    for (const currId of cardIdsToDuplicate) {
+      scratchcardResolutions[currId - 1].numberOfOccurences += scratchcardResolution.numberOfOccurences;
+    };
+  };
+
+  return scratchcardResolutions.map(scratchcardResolution => scratchcardResolution.numberOfOccurences)
     .reduce((acc, curr) => acc + curr, 0);
 }
 
-export const calcScratchcardValue = (scratchcard: Scratchcard): number => {
-  const validNumbers: number = scratchcard.gameNumbers.filter(gameNumber => scratchcard.winningNumbers.includes(gameNumber)).length - 1;
-  return validNumbers >= 0 ? 2 ** validNumbers : 0;
+export const calcCardIdsToDuplicate = (scratchcard: Scratchcard): number[] => {
+  return [...Array(calcScratchcardWinningNumbersCount(scratchcard)).keys()].map(cardId => cardId + scratchcard.cardId + 1)
+}
+
+export const calcScratchcardWinningNumbersCount = (scratchcard: Scratchcard): number => {
+  const validNumbers: number = scratchcard.gameNumbers.filter(gameNumber => scratchcard.winningNumbers.includes(gameNumber)).length;
+  return validNumbers ?? 0;
 }
 
 export const parseScratchcard = (game: string): Scratchcard => {
   const [gameId, gameValues] = game.split(':');
   const [winningString, gameString] = gameValues.split('|');
 
-  const winningNumbers = parseStringToArrayNumber(winningString, ' ');
-  const gameNumbers = parseStringToArrayNumber(gameString, ' ');
-
   return {
-    winningNumbers, gameNumbers
+    cardId: parseStringToArrayNumber(gameId, ' ')[0],
+    winningNumbers: parseStringToArrayNumber(winningString, ' '), 
+    gameNumbers: parseStringToArrayNumber(gameString, ' ')
   }
 }
 
@@ -38,4 +62,5 @@ const loadFile = (filename: string): string => {
   return fs.readFileSync(`${__dirname}/${filename}`).toString();
 }
 
-console.log('input.in : ', solve(loadFile('input.in')));
+console.log('part1 : ', solve1(loadFile('input.in')));
+console.log('part2 : ', solve2(loadFile('input.in')));
